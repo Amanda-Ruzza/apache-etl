7cf6638a0318e2865dcfa26209b2d4e05a62e540
Amanda-Ruzza <amanda.ruzza@cloudreach.com>
2023-06-23 16:16:44 -0400

    Add Simple Transforms

--- apache_pipeline.py
+++ apache_pipeline.py
@@ -1,3 +1,6 @@
+import logging
+import argparse
+import re # find the regular expressions for the unnecessary words to remove
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 
@@ -13,11 +16,17 @@ def run_pipeline():
         # Apply transformations to process the data
         processed_data = (
             data
+            
             | 'TransformData' >> beam.Map(lambda x: x.upper())
+            | 'Strip' >> beam.Map(lambda x: x.strip(','))
+            |'Strip header' >> beam.Map(lambda text: text.strip('# \n'))
         )
         
         # Write the processed data to a sink (e.g., text file)
         processed_data | 'WriteToText' >> beam.io.WriteToText('apache_strength_log.csv')
+        processed_data | 'PrintToConsole' >> beam.Map(print)
+
 
 if __name__ == '__main__':
+    logging.getLogger().setLevel(logging.INFO)
     run_pipeline()
