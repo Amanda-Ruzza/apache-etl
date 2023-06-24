1d2f78b70c93c906171821ad6f8323531db761f6
Amanda-Ruzza <amanda.ruzza@cloudreach.com>
2023-06-23 20:00:26 -0400

    Add Argument Parser for CLI I/O + Logger

--- apache_pipeline.py
+++ apache_pipeline.py
@@ -4,17 +4,33 @@ import re # find the regular expressions for the unnecessary words to remove
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 
-def run_pipeline():
-    pipeline_options = PipelineOptions()
-    with beam.Pipeline(options=pipeline_options) as pipeline:
-        # Read data from a source (e.g., CSV file)
-        data = (
+def run_pipeline(argv=None):
+
+  """Main entry point; defines and runs the wordcount pipeline."""
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+    '--input',
+    dest='input',
+    help='Input file to process.')
+  parser.add_argument(
+    '--output',
+    dest='output',
+    required=True,
+    help='Output file to write results to.')
+  known_args, pipeline_args = parser.parse_known_args(argv)
+
+  pipeline_options = PipelineOptions(pipeline_args)
+
+  # The pipeline will be run on exiting the 'with' block.    
+  with beam.Pipeline(options=pipeline_options) as pipeline:
+    # Read data from a source (e.g., CSV file)
+    data = (
             pipeline
             | 'ReadFromCSV' >> beam.io.ReadFromText('StrengthLog.csv')
         )
         
-        # Apply transformations to process the data
-        processed_data = (
+    # Apply transformations to process the data
+    processed_data = (
             data
             
             | 'TransformData' >> beam.Map(lambda x: x.upper())
@@ -23,10 +39,12 @@ def run_pipeline():
         )
         
         # Write the processed data to a sink (e.g., text file)
-        processed_data | 'WriteToText' >> beam.io.WriteToText('apache_strength_log.csv')
-        processed_data | 'PrintToConsole' >> beam.Map(print)
+    processed_data | 'WriteToText' >> beam.io.WriteToText(known_args.output)
+    processed_data | 'PrintToConsole' >> beam.Map(print)
+
+
 
 
 if __name__ == '__main__':
-    logging.getLogger().setLevel(logging.INFO)
-    run_pipeline()
+  logging.getLogger().setLevel(logging.INFO)
+  run_pipeline()
